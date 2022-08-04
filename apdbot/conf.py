06ee8bf6f730773e69bf172037acc946078c695b
DeployerFred <99484705+DeployerFred@users.noreply.github.com>
2022-08-04 19:25:37 +0300

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -4,6 +4,6 @@ from apdbot.sample_con import Config
 class Config(Config):
     BOT_TOKEN= "5525673460:AAH_j9KI6-objMaOtMLUhwCaClbfif8Ga28" 
     APP_ID = 18009375 
-    API_HASH = "18009375" 
+    API_HASH = "1c6b8b0a259aa35affee58377c634eeb" 
     OWNER_ID = "1528709600" 
     AUTH = [-671555448]
